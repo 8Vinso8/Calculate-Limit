7d150cc9d8497e9ebd2a51884d75c561cdf93195
Vinso <8vinso8@gmail.com>
2022-10-04 16:21:09 +1000

    Add user input of e

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <iostream>
+#include <string>
 #include <iomanip>
 
 long double Formula(long long int n){
@@ -8,13 +9,27 @@ long double Formula(long long int n){
 
 using namespace std;
 int main(){
-  long double e = 0.0000001;
+  cout << "Введите e: ";
+  string e_str;
+  cin >> e_str;
+  cout << endl;
+  int precision = e_str.length() - 2;
+  long double e;
+  try
+  {
+    e = stod(e_str);
+  }
+  catch(const std::exception& except)
+  {
+    std::cerr << except.what() << '\n';
+    return 0;
+  }
   long double b = 0.0;
   long double iter = e;
   long long int n = 1;
   long double Sn = 0;
   long long int count = 0;
-  int max_allowed = 100;
+  int max_allowed = 1000;
   while(true){
     Sn += Formula(n);
     if (abs(Sn - b) < e)
@@ -28,7 +43,7 @@ int main(){
     n++;
   }
   cout << fixed;
-  cout << setprecision(7);
+  cout << setprecision(precision);
   cout << b << endl;
   return 0;
 }
